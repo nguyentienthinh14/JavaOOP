07670a8a0b2ed0770a05fa1819d40f2657eebebb
nguyentienthinh14 <nguyenthinh141204@gmail.com>
2024-01-19 22:01:07 +0700

    PhuongTrinhBac1

--- BaiThucHanh/src/PhuongTrinhBac1.java
+++ BaiThucHanh/src/PhuongTrinhBac1.java
@@ -1,3 +1,26 @@
+import java.util.Scanner;
+
 public class PhuongTrinhBac1 {
-    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Nhap a: ");
+        double a = scanner.nextDouble();
+
+        System.out.print("Nhap b: ");
+        double b = scanner.nextDouble();
+
+        scanner.close();
+        if (a == 0) {
+            if (b == 0) {
+                System.out.print("Phuong trinh co vo so nghiem");
+            } else {
+                System.out.print("Phuong trinh vo nghiem");
+            }
+        } else {
+            double x = -b / a;
+            System.out.print("Nghiem cua phuong trinh la: x = " +x);
+        }
+        scanner.close();
+    }
 }
