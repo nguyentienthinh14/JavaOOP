38b9437cb72e7ce38ad3bb78d2ebf22df4226a92
nguyentienthinh14 <nguyenthinh141204@gmail.com>
2024-02-01 17:11:35 +0700

    Slide30Chuong2

--- BaiThucHanh/src/Slide30Chuong2.java
+++ BaiThucHanh/src/Slide30Chuong2.java
@@ -4,7 +4,7 @@ public class Slide30Chuong2 {
     public static void main(String[] args) {
         int n, SoDu, Tong = 0;
         Scanner sc = new Scanner(System.in);
-        System.out.print("Nhap so nguyen n: ");
+        System.out.print("Nhap so nguyen n: "); //nhap
         n = sc.nextInt();
         while (n>0) {
             SoDu= n%10 ;
